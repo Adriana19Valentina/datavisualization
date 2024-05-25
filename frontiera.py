b08de2e7d54e76e7049a5f7fa8de3e1b6129c091
Costache Adriana <38680854+Adriana19Valentina@users.noreply.github.com>
2024-05-25 13:40:04 +0300

    Update frontiera.py

--- frontiera.py
+++ frontiera.py
@@ -13,10 +13,7 @@ df = df_cleaned
 df["geometry"] = df.apply(lambda row: Point(row["longitude"], row["latitude"]), axis=1)
 geo_df = gpd.GeoDataFrame(df, geometry="geometry")
 
-# Set the initial CRS for geo_df to WGS 84
 geo_df.set_crs(epsg=4326, inplace=True)
-
-# Assuming you have a shapefile for Romania's boundaries
 romania = gpd.read_file("ro_frontiera_poligon.shp")
 
 
@@ -27,11 +24,9 @@ if geo_df.crs != romania.crs:
 # Perform spatial join to filter out only points within Romania
 data_within_romania = gpd.sjoin(geo_df, romania, how="inner", op="within")
 
-# Optionally, drop the geometry column if it's no longer needed
+#drop the geometry column if it's no longer needed
 data_within_romania = data_within_romania.drop(columns=["geometry"])
 
 # Save the cleaned, deduplicated, and spatially filtered data to a new CSV file
 data_within_romania.to_csv("data_within_romania_3.csv", index=False)
-
-# Print the first few rows of the final dataset to verify
 print(data_within_romania.head())
